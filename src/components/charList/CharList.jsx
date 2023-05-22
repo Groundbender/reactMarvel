8d1b3c0ca4a70266f0269592cebe5fe57e50df73
Damir <damir24may@mail.ru>
2023-05-23 02:40:34 +0300

    added charList and error and loading states

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,52 +1,113 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from "react";
+import "./charList.scss";
+import abyss from "../../resources/img/abyss.jpg";
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/spinner";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+class CharList extends Component {
+  state = {
+    charList: null,
+    loading: true,
+    error: false,
+  };
 
-const CharList = () => {
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.marvelService
+      .getAllCharacters()
+      .then((charList) => this.onCharListLoaded(charList))
+      .catch(this.onError);
+  }
+
+  onCharListLoaded = (charList) => {
+    this.setState({ charList, loading: false });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  render() {
+    const { charList, loading, error } = this.state;
+
+    const contain = {
+      objectFit: "contain",
+    };
+
+    const centeredLoader = {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    };
+
+    const cards = charList ? (
+      charList.map(({ name, thumbnail, id }) => {
+        return (
+          <li key={id} className="char__item">
+            <img
+              src={thumbnail}
+              alt="abyss"
+              style={thumbnail.endsWith("not_available.jpg") ? contain : null}
+            />
+            <div className="char__name">{name}</div>
+          </li>
+        );
+      })
+    ) : (
+      <Spinner />
+    );
+
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <div className="char__list">
+        <ul className="char__grid" style={loading ? centeredLoader : null}>
+          {cards}
+          {/* <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item char__item_selected">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li>
+          <li className="char__item">
+            <img src={abyss} alt="abyss" />
+            <div className="char__name">Abyss</div>
+          </li> */}
+        </ul>
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default CharList;
--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -5,11 +5,6 @@ import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/spinner";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 class RandomChar extends Component {
-  constructor(props) {
-    super(props);
-    this.updateChar();
-  }
-
   state = {
     char: {},
     loading: true,
@@ -18,8 +13,21 @@ class RandomChar extends Component {
 
   marvelService = new MarvelService();
 
+  componentDidMount() {
+    // this.timerId = setInterval(this.updateChar, 3000);
+    this.updateChar();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   onCharLoaded = (char) => {
     this.setState({ char, loading: false });
+  };
+
+  onCharLoading = () => {
+    this.setState({ loading: true });
   };
 
   onError = () => {
@@ -31,6 +39,7 @@ class RandomChar extends Component {
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
 
     this.marvelService
       .getCharacter(id)
@@ -57,7 +66,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button onClick={this.updateChar} className="button button__main">
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -80,9 +89,20 @@ const View = ({ char }) => {
       ? validDescription.slice(0, DESCRIPTION_NORMAL_LENGTH) + "..."
       : validDescription;
 
+  // thumbnail = thumbnail.endsWith("not_available.jpg") ?
+
+  const contain = {
+    objectFit: "contain",
+  };
+
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={thumbnail.endsWith("not_available.jpg") ? contain : null}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{validAndSlicedDescription}</p>
