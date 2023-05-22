89006c6a1e7696339b78c73e94266092457a92ee
Damir <damir24may@mail.ru>
2023-05-22 15:22:42 +0300

    added new state, fetch character and characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,14 +12,33 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    // делаем то же, что и для отдельного персонажа, но для каждого персонажа с помощбю map и формируем новый массив
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacter = async (id) => {
+    // получаем данные
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+
+    // возвращаем только нужные данные в собранном объекте через ф-ию    _transformCharacter
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  // char = res.data.results[0]
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
